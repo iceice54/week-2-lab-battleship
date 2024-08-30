b64a6b9d036f190ec470a5ca24887755317bdfde
kj <125724815+iceice54@users.noreply.github.com>
2024-08-30 15:44:14 +0800

    initial

---
+++ lab wk 2 battleship.py
@@ -0,0 +1,38 @@
+class Board:
+    def __init__(self):
+        self.placement = [([0] * 10)] * 10
+        self.hit = [([0] * 10)] * 10
+        self.placeShip("carrier")
+        self.placeShip("submarine")
+    
+    def placeShip(self, ship):
+        if ship == "carrier":
+            shipLength = 4
+        elif ship == "submarine":
+            shipLength = 3
+
+        try:
+            orientation, row, column = input("Enter info: \"orientation,row,column\": ").split(",")
+        except:
+            raise
+        
+        if orientation == "h":
+            print(type(row))
+            for i in self.placement[int(row)]:
+                print("hi")
+        elif orientation == "v":
+            pass
+        else:
+            raise ValueError("invalid orientation")
+
+        print(orientation)
+        print(row)
+        print(column)
+        print(shipLength)
+
+def printBoard(board):
+    for i in board:
+        print (i)
+
+p1Board = Board()
+# printBoard(p1Board.placement)
